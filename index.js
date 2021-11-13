d31dcb4707bcbb37671d86cd2ff73fcc9747f98c
yeasirarafatpp <yeasira79@gmail.com>
2021-11-13 15:50:10 +0600

    going to live

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
 const port = process.env.PORT || 5000;
 
+// MiddleWare
+app.use(cors());
+app.use(express.json());
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wdoqy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("AllSSD");
+        const ssdCollection = database.collection("ssd");
+
+        // GET ALL SSD
+        app.get('/products', async (req, res) => {
+            const cursor = ssdCollection.find({});
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hi I am database');
